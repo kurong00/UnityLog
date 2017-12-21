5e17564ad1bbdbd9aa721ae8451a4e7873de3bf3
kurong00 <kurong00@gmail.com>
2017-12-21 17:13:21 +0800

    edit LogInformation

--- Assets/XDebug/LogInformation.cs
+++ Assets/XDebug/LogInformation.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Diagnostics;
+using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 public enum LogLevel
 {
@@ -8,16 +11,32 @@ public enum LogLevel
     Warning,
     Error,
 }
+[Serializable]
 public class LogInformation
 {
-
-    Object Origin;
+    UnityEngine.Object Origin;
     string Channel;
     List<LogStackFrame> StackFrameList;
+    LogStackFrame originStackFrame;
     LogLevel logLevel;
 
     string Message;
     double RelativeTimeLine;
     double AbsoluteTimeLine;
-    
+
+    public LogInformation(UnityEngine.Object origin,string channel,LogLevel level,
+        List<LogStackFrame> stackFrameList,LogStackFrame logStackFrame, System.Object message)
+    {
+        Origin = origin;
+        Channel = channel;
+        logLevel = level;
+        StackFrameList = stackFrameList;
+        originStackFrame = logStackFrame;
+        var formatMessage = message as String;
+        if(formatMessage != null)
+        {
+            Message = formatMessage;
+        }
+        originStackFrame = logStackFrame;
+    }
 }
